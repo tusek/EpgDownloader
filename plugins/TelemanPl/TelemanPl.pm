bb08ae81e762f7596a7d2b8e6a2f638eb1c1fcb0
Artur Śmiałek <Artur.Smialek@gmail.com>
2012-01-18 13:29:28 +0100

    Avoid shows at programme with too many repetitions - for speed up full description grabing

--- plugins/TelemanPl/TelemanPl.pm
+++ plugins/TelemanPl/TelemanPl.pm
@@ -112,7 +112,8 @@ sub get {
           #@todo From version 1.50 of WWW-Mechanize content is decoded by default. For now we have to handle it this way.
           #my $tmp = $browser->content();
           my $tmp = $browser->response()->decoded_content();	
-          if ( $descriptionUrl ne "/tv/Zakonczenie-Programu-41")
+          if ( $descriptionUrl ne "/tv/Zakonczenie-Programu-41" && $descriptionUrl ne "/tv/Serwis-Info-228001"
+                && $descriptionUrl ne "/tv/Pogoda-228002" )
           {
                 $description = $1 if $tmp =~ /.*?<p property="v:summary" class="summary">(.*?)<\/p>.*/;
           }
